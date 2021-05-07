0a39410d284ee38b0079875ecac0313313a134a8
shijie03 <shijie03@shengqugames.com>
2021-05-07 18:51:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,15 @@ set -e
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
-# 提交到 master 分支
-# git push origin master
 
-# 将 dist 文件提交到 gh-pages 分支
-# git subtree push --prefix dist origin gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:SHIJIECHN/front-end-note.git master:gh-pages
